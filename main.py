86416636e0326127dd0485f33dd070ea8f0e10c3
himatvarsani-cloudreach <himat.varsani@cloudreach.com>
2021-11-11 17:05:37 +0000

    initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#!//Users/himat.varsani/.pyenv/shims/python
+
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/hello")
+def world():
+    return "<h1>Hello World!!!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1')
